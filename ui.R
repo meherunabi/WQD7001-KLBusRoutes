1d20b7f77bac778811c9c961ed805aa869853c2b
meherunabi <48278556+meherunabi@users.noreply.github.com>
2019-12-08 15:29:55 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,18 +1,13 @@
+ibrary(shiny)
 
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-library(shiny)
-# Define UI for application
-
+#UI section -------
 fluidPage(
-  titlePanel("WQD7001 - Group project"),
-  textInput(inputId = "inputTextSearch", label = "Search location", width = "50%", value = ""),
-  textOutput(outputId = "outputTextSearch"),
-  dataTableOutput(outputId = "outputDataTable")
-)
+  titlePanel( title="Customer Information"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput ("location","Enter Location", choices = my_data$Area)
+    ),
+    mainPanel(
+      tableOutput("mylocation")
+    )
+  ))
